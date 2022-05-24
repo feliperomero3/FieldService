8cce0b7e303da29e56520fdc48c85e698343b01d
Felipe Romero <feliperomero3@outlook.com>
2022-05-24 18:54:17 -0500

    Only enable HSTS in production environment.
    HSTS is very sticky!

--- src/Program.cs
+++ src/Program.cs
@@ -12,7 +12,7 @@ public class Program
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
-        if (!app.Environment.IsDevelopment())
+        if (app.Environment.IsProduction())
         {
             app.UseExceptionHandler("/Error");
             // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
