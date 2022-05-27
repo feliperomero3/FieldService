1413014f16deed5955e16e59491e6e7acbb54b34
Felipe Romero <feliperomero3@outlook.com>
2022-05-26 22:13:38 -0500

    Add Entity class.

---
+++ src/Entity.cs
@@ -0,0 +1,72 @@
+﻿// Customized from https://github.com/vkhorikov/DddAndEFCore/blob/90bf08dcaa8f9ab000713060effa8a7f46a303e1/src/App/Entity.cs
+
+namespace FieldService;
+
+public abstract class Entity
+{
+    public long Id { get; }
+
+    protected Entity()
+    {
+    }
+
+    protected Entity(long id)
+        : this()
+    {
+        Id = id;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is not Entity other)
+            return false;
+
+        if (ReferenceEquals(this, other))
+            return true;
+
+        if (GetRealType() != other.GetRealType())
+            return false;
+
+        if (Id == 0 || other.Id == 0)
+            return false;
+
+        return Id == other.Id;
+    }
+
+    public static bool operator ==(Entity a, Entity b)
+    {
+        if (a is null && b is null)
+            return true;
+
+        if (a is null || b is null)
+            return false;
+
+        return a.Equals(b);
+    }
+
+    public static bool operator !=(Entity a, Entity b)
+    {
+        return !(a == b);
+    }
+
+    public override int GetHashCode()
+    {
+        return (GetRealType().ToString() + Id).GetHashCode();
+    }
+
+    private Type GetRealType()
+    {
+        const string EfCoreProxyPrefix = "Castle.Proxies.";
+
+        var type = GetType();
+
+        var typeString = type.ToString();
+
+        if (typeString != null && type.BaseType != null)
+        {
+            return typeString.Contains(EfCoreProxyPrefix) ? type.BaseType : type;
+        }
+
+        return type;
+    }
+}
